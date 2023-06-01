19a19798d1cf14b851da7753aec86f63c4956e35
kindy235 <bahaboubacar03@gmail.com>
2023-06-01 17:10:02 +0200

    login

---
+++ Api/target/classes/static/js/cards-list.js
@@ -0,0 +1,45 @@
+
+
+
+async function getCardsList() {
+    let cardList = [];
+    let url = '/cards';
+
+    let template = document.querySelector("#row");
+    fetch(url, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+          'Access-Control-Allow-Headers': 'Content-Type'
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          data.forEach(card => {
+            let clone = document.importNode(template.content, true);
+
+            newContent = clone.firstElementChild.innerHTML
+                .replace(/{{img_src}}/g, "card.png")
+                .replace(/{{color}}/g, card.color)
+                .replace(/{{number}}/g, card.number)
+                .replace(/{{price}}/g, card.price);
+            clone.firstElementChild.innerHTML = newContent;
+        
+            let cardContainer = document.querySelector("#tableContent");
+            cardContainer.appendChild(clone);
+          });
+
+        })
+        .catch(error => {
+          console.log('Error:', error);
+        });
+      
+    $(function () {
+    $("#card").load("./part/card-show.html");
+    });
+}
+
+getCardsList();
